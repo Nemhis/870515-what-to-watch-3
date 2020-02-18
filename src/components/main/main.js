dc2737b315b308a0bd0b42763f814623d9054855
Nemhis <p.nemphis@gmail.com>
2020-02-18 22:02:01 +0300

    review fixes

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,19 +1,16 @@
 import React from 'react';
-
-const formatDate = (date) => `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+import {formatDate} from '../../utils';
 
 // eslint-disable-next-line react/prop-types
 const Main = ({film}) => {
   // eslint-disable-next-line react/prop-types
   const {name, genre, promoDate} = film;
 
-  return <React.Fragment>
-    <div>
-      <div><b>Название:</b> {name}</div>
-      <div><b>Жанр:</b> {genre}</div>
-      <div><b>Дата выхода промо:</b> {formatDate(promoDate)}</div>
-    </div>
-  </React.Fragment>;
+  return <div>
+    <div><b>Название:</b> {name}</div>
+    <div><b>Жанр:</b> {genre}</div>
+    <div><b>Дата выхода промо:</b> {formatDate(promoDate)}</div>
+  </div>;
 };
 
 export default Main;
