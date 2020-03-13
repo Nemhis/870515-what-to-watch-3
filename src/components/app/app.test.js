ad9c4f5e62159195f6e3ad8d84766deaaedab7b1
Nemhis <p.nemphis@gmail.com>
2020-03-13 15:03:23 +0300

    <App> snapshot test

---
+++ src/components/app/app.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './app';
+
+const films = [`Test film 1`, `Test film 2`];
+
+describe(`<App>`, () => {
+  it(`Render <App>`, () => {
+    const tree = renderer
+      .create(<App films={films} />)
+      .toJSON(`test`);
+
+    expect(tree).toMatchSnapshot();
+  });
+});
