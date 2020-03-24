d93dbf15bbac560d07df34b2713b1238c8b18881
Nemhis <p.nemphis@gmail.com>
2020-03-24 16:47:01 +0300

    Add router to <App/>

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Main from '../main/main.jsx';
+import FilmDetailInfo from '../film-detail-info/film-detail-info.jsx';
+
+const _renderApp = (films) => {
+  return <Main films={films} />;
+};
 
 const App = ({films}) => {
-  return <Main films={films} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {_renderApp(films)}
+        </Route>
+        <Route exact path="/dev-film-detail-info">
+          <FilmDetailInfo film={films[0]} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
