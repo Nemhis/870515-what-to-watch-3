80a6064c13671da4e628a35e51042941b6744eb8
Nemhis <p.nemphis@gmail.com>
2020-03-26 21:24:06 +0300

    <VideoPlayer> test playing state for video

---
+++ src/components/video-player/video-player.e2e.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import VideoPlayer from './video-player';
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+const film = {
+  previewImg: `/picture/src.jpg`,
+  previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+};
+
+describe(`<VideoPlayer> e2e tests`, () => {
+  it(`VideoPlayer should play video`, () => {
+    const onPlay = jest.fn();
+    const onLoad = jest.fn();
+
+    const videoPlayerWrapper = shallow(
+        <VideoPlayer
+          src={film.previewVideoLink}
+          poster={film.previewImg}
+          isPlaying={false}
+        />
+    );
+
+    videoPlayerWrapper.instance()._videoRef.current = {
+      play() {
+        onPlay();
+      },
+      load() {
+        onLoad();
+      },
+    };
+
+    videoPlayerWrapper.setProps({
+      isPlaying: true,
+    });
+
+    expect(onPlay.mock.calls.length).toBe(1);
+
+    videoPlayerWrapper.setProps({
+      isPlaying: false,
+    });
+
+    expect(onLoad.mock.calls.length).toBe(1);
+  });
+});
