cf186756aec6467032032f5e3ae94c8dda040b0a
Nemhis <p.nemphis@gmail.com>
2020-03-28 20:36:31 +0300

    Tabs usage

--- src/components/film-detail-info/film-detail-info.jsx
+++ src/components/film-detail-info/film-detail-info.jsx
@@ -1,202 +1,184 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
-const getTextEstimation = (rating) => {
-  rating = rating >= 0 && rating <= 10 ? rating : -1;
-  let textEstimation = ``;
+import Tabs from '../tabs/tabs.jsx';
+import FilmOverview from '../film-overview/film-overview.jsx';
+import FilmDetails from '../film-details/film-details.jsx';
+import FilmReviews from '../film-reviews/film-reviews.jsx';
 
-  switch (true) {
-    case rating > 0 && rating < 3:
-      textEstimation = `Bad`;
-      break;
-    case rating < 5:
-      textEstimation = `Normal`;
-      break;
-    case rating < 8:
-      textEstimation = `Good`;
-      break;
-    case rating < 10:
-      textEstimation = `Very good`;
-      break;
-    case rating === 10:
-      textEstimation = `Awesome`;
-      break;
+class FilmDetailInfo extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTabIndex: 0,
+      tabs: this._createTabs(),
+    };
   }
 
-  return textEstimation;
-};
+  _createTabs() {
+    return [
+      {
+        name: `Overview`,
+        component: FilmOverview,
+      },
+      {
+        name: `Details`,
+        component: FilmDetails,
+      },
+      {
+        name: `Reviews`,
+        component: FilmReviews,
+      }
+    ];
+  }
 
-const FilmDetailInfo = ({film, onNameClick, onPosterClick}) => {
-  const {
-    backgroundImg,
-    posterImg,
-    name,
-    genre,
-    released,
-    rating,
-    scoresCount,
-    description,
-    director,
-    starring
-  } = film;
+  render() {
+    const {film, onNameClick, onPosterClick} = this.props;
+    const {activeTabIndex, tabs} = this.state;
+    const ActiveTabComponent = tabs[activeTabIndex].component;
+    const {
+      backgroundImg,
+      posterImg,
+      name,
+      genre,
+      released,
+    } = film;
 
-  return (
-    <React.Fragment>
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__hero">
-          <div className="movie-card__bg">
-            <img src={backgroundImg} alt="The Grand Budapest Hotel"/>
+    return (
+      <React.Fragment>
+        <section className="movie-card movie-card--full">
+          <div className="movie-card__hero">
+            <div className="movie-card__bg">
+              <img src={backgroundImg} alt="The Grand Budapest Hotel"/>
+            </div>
+
+            <h1 className="visually-hidden">WTW</h1>
+
+            <header className="page-header movie-card__head">
+              <div className="logo">
+                <a href="main.html" className="logo__link">
+                  <span className="logo__letter logo__letter--1">W</span>
+                  <span className="logo__letter logo__letter--2">T</span>
+                  <span className="logo__letter logo__letter--3">W</span>
+                </a>
+              </div>
+
+              <div className="user-block">
+                <div className="user-block__avatar">
+                  <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+                </div>
+              </div>
+            </header>
+
+            <div className="movie-card__wrap">
+              <div className="movie-card__desc">
+                <h2 className="movie-card__title" onClick={onNameClick}>{name}</h2>
+                <p className="movie-card__meta">
+                  <span className="movie-card__genre">{genre}</span>
+                  <span className="movie-card__year">{released}</span>
+                </p>
+
+                <div className="movie-card__buttons">
+                  <button className="btn btn--play movie-card__button" type="button">
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s"></use>
+                    </svg>
+                    <span>Play</span>
+                  </button>
+                  <button className="btn btn--list movie-card__button" type="button">
+                    <svg viewBox="0 0 19 20" width="19" height="20">
+                      <use xlinkHref="#add"></use>
+                    </svg>
+                    <span>My list</span>
+                  </button>
+                  <a href="add-review.html" className="btn movie-card__button">Add review</a>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <h1 className="visually-hidden">WTW</h1>
+          <div className="movie-card__wrap movie-card__translate-top">
+            <div className="movie-card__info">
+              <div className="movie-card__poster movie-card__poster--big">
+                <img src={posterImg} alt="The Grand Budapest Hotel poster" width="218"
+                  height="327" onClick={onPosterClick}/>
+              </div>
 
-          <header className="page-header movie-card__head">
+              <div className="movie-card__desc">
+                <Tabs activeTabIndex={activeTabIndex} tabs={tabs} onTabClick={(tabIndex) => {
+                  this.setState({activeTabIndex: tabIndex});
+                }}/>
+
+                <ActiveTabComponent film={film} />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div className="page-content">
+          <section className="catalog catalog--like-this">
+            <h2 className="catalog__title">More like this</h2>
+
+            <div className="catalog__movies-list">
+              <article className="small-movie-card catalog__movies-card">
+                <div className="small-movie-card__image">
+                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
+                    alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
+                </div>
+                <h3 className="small-movie-card__title">
+                  <a className="small-movie-card__link" href="movie-page.html">Fantastic Beasts: The Crimes of
+                    Grindelwald</a>
+                </h3>
+              </article>
+
+              <article className="small-movie-card catalog__movies-card">
+                <div className="small-movie-card__image">
+                  <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175"/>
+                </div>
+                <h3 className="small-movie-card__title">
+                  <a className="small-movie-card__link" href="movie-page.html">Bohemian Rhapsody</a>
+                </h3>
+              </article>
+
+              <article className="small-movie-card catalog__movies-card">
+                <div className="small-movie-card__image">
+                  <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175"/>
+                </div>
+                <h3 className="small-movie-card__title">
+                  <a className="small-movie-card__link" href="movie-page.html">Macbeth</a>
+                </h3>
+              </article>
+
+              <article className="small-movie-card catalog__movies-card">
+                <div className="small-movie-card__image">
+                  <img src="img/aviator.jpg" alt="Aviator" width="280" height="175"/>
+                </div>
+                <h3 className="small-movie-card__title">
+                  <a className="small-movie-card__link" href="movie-page.html">Aviator</a>
+                </h3>
+              </article>
+            </div>
+          </section>
+
+          <footer className="page-footer">
             <div className="logo">
-              <a href="main.html" className="logo__link">
+              <a href="main.html" className="logo__link logo__link--light">
                 <span className="logo__letter logo__letter--1">W</span>
                 <span className="logo__letter logo__letter--2">T</span>
                 <span className="logo__letter logo__letter--3">W</span>
               </a>
             </div>
 
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-              </div>
+            <div className="copyright">
+              <p>© 2019 What to watch Ltd.</p>
             </div>
-          </header>
-
-          <div className="movie-card__wrap">
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title" onClick={onNameClick}>{name}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{genre}</span>
-                <span className="movie-card__year">{released}</span>
-              </p>
-
-              <div className="movie-card__buttons">
-                <button className="btn btn--play movie-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list movie-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-                <a href="add-review.html" className="btn movie-card__button">Add review</a>
-              </div>
-            </div>
-          </div>
+          </footer>
         </div>
-
-        <div className="movie-card__wrap movie-card__translate-top">
-          <div className="movie-card__info">
-            <div className="movie-card__poster movie-card__poster--big">
-              <img src={posterImg} alt="The Grand Budapest Hotel poster" width="218"
-                height="327" onClick={onPosterClick}/>
-            </div>
-
-            <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-
-              <div className="movie-rating">
-                <div className="movie-rating__score">{rating}</div>
-                <p className="movie-rating__meta">
-                  <span className="movie-rating__level">{getTextEstimation(rating)}</span>
-                  <span className="movie-rating__count">{scoresCount} ratings</span>
-                </p>
-              </div>
-
-              <div className="movie-card__text">
-                <p>{description}</p>
-
-                <p className="movie-card__director"><strong>Director: {director}</strong></p>
-
-                <p className="movie-card__starring"><strong>Starring: {starring}</strong></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <div className="page-content">
-        <section className="catalog catalog--like-this">
-          <h2 className="catalog__title">More like this</h2>
-
-          <div className="catalog__movies-list">
-            <article className="small-movie-card catalog__movies-card">
-              <div className="small-movie-card__image">
-                <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
-                  alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
-              </div>
-              <h3 className="small-movie-card__title">
-                <a className="small-movie-card__link" href="movie-page.html">Fantastic Beasts: The Crimes of
-                  Grindelwald</a>
-              </h3>
-            </article>
-
-            <article className="small-movie-card catalog__movies-card">
-              <div className="small-movie-card__image">
-                <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175"/>
-              </div>
-              <h3 className="small-movie-card__title">
-                <a className="small-movie-card__link" href="movie-page.html">Bohemian Rhapsody</a>
-              </h3>
-            </article>
-
-            <article className="small-movie-card catalog__movies-card">
-              <div className="small-movie-card__image">
-                <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175"/>
-              </div>
-              <h3 className="small-movie-card__title">
-                <a className="small-movie-card__link" href="movie-page.html">Macbeth</a>
-              </h3>
-            </article>
-
-            <article className="small-movie-card catalog__movies-card">
-              <div className="small-movie-card__image">
-                <img src="img/aviator.jpg" alt="Aviator" width="280" height="175"/>
-              </div>
-              <h3 className="small-movie-card__title">
-                <a className="small-movie-card__link" href="movie-page.html">Aviator</a>
-              </h3>
-            </article>
-          </div>
-        </section>
-
-        <footer className="page-footer">
-          <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
-      </div>
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
 
 FilmDetailInfo.defaultProps = {
   onNameClick: () => {},
@@ -210,11 +192,6 @@ FilmDetailInfo.propTypes = {
     backgroundImg: PropTypes.string.isRequired,
     genre: PropTypes.string.isRequired,
     released: PropTypes.number.isRequired,
-    rating: PropTypes.number.isRequired,
-    scoresCount: PropTypes.number.isRequired,
-    description: PropTypes.string.isRequired,
-    director: PropTypes.string.isRequired,
-    starring: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
 
   onNameClick: PropTypes.func,
