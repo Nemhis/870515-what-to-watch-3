ff6872c722edcff33c0a185ad8af20c829fe2594
Nemhis <p.nemphis@gmail.com>
2020-03-28 18:32:43 +0300

    <Tabs> component

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Tabs = ({tabs, onTabClick, activeTabIndex}) => {
+  return (
+    <nav className="movie-nav movie-card__nav">
+      <ul className="movie-nav__list">
+        {tabs.map(({name}, index) => (
+          <li key={index} className={`movie-nav__item ${index === activeTabIndex ? `movie-nav__item--active` : ``}`}>
+            <a href="#" className="movie-nav__link" onClick={() => {
+              onTabClick(index);
+            }}>
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Tabs.propTypes = {
+  activeTabIndex: PropTypes.number.isRequired,
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+  onTabClick: PropTypes.func.isRequired,
+};
+
+export default Tabs;
