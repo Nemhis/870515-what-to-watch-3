da0b2df7f93eb909b4f1f8102e0de5c7f3e6434b
Nemhis <p.nemphis@gmail.com>
2020-03-31 16:39:24 +0300

    Reducer tests

---
+++ src/reducer.test.js
@@ -0,0 +1,155 @@
+import {reducer} from './reducer.js';
+import {ActionType, DEFAULT_GENRE_ITEM} from './actions';
+
+const films = [
+  {
+    id: 1,
+    name: `The Grand Budapest Hotel`,
+    posterImg: `img/the-grand-budapest-hotel-poster.jpg`,
+    previewImg: `img/the-grand-budapest-hotel.jpg`,
+    backgroundImg: `img/bg-the-grand-budapest-hotel.jpg`,
+    backgroundColor: `#ffffff`,
+    videoLink: `https://some-link`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+    rating: 8.9,
+    scoresCount: 240,
+    director: `Wes Andreson`,
+    starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
+    runTime: 99,
+    genre: `Comedy`,
+    released: 2014,
+    isFavorite: false
+  },
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `Comedy`,
+    previewImg: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Bohemian Rhapsody`,
+    genre: `Comedy`,
+    previewImg: `img/bohemian-rhapsody.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Macbeth`,
+    genre: `Crime`,
+    previewImg: `img/macbeth.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Aviator`,
+    genre: `Documentary`,
+    previewImg: `img/aviator.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `We need to talk about Kevin`,
+    genre: `Horror`,
+    previewImg: `img/we-need-to-talk-about-kevin.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `What We Do in the Shadows`,
+    genre: `Romance`,
+    previewImg: `img/what-we-do-in-the-shadows.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Revenant`,
+    genre: `Sci-Fi`,
+    previewImg: `img/revenant.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Johnny English`,
+    genre: `Thrillers`,
+    previewImg: `img/johnny-english.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `Kids & Family`,
+    previewImg: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `Documentary`,
+    previewImg: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `Test genre 1`,
+    previewImg: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  },
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `Test genre 2`,
+    previewImg: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    previewVideoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+  }
+];
+
+const genres = [DEFAULT_GENRE_ITEM, `Comedy`, `Crime`, `Documentary`,
+  `Horror`, `Romance`, `Sci-Fi`, `Thrillers`, `Kids & Family`, `Test genre 1`];
+
+describe(`Reducer tests`, () => {
+  it(`Default store state`, () => {
+    expect(reducer(void 0, {}))
+      .toEqual({
+        films,
+        genres,
+        selectedGenre: DEFAULT_GENRE_ITEM,
+      });
+  });
+
+  it(`Reducer should change genre filter`, () => {
+    expect(reducer({
+      films,
+      genres,
+      selectedGenre: DEFAULT_GENRE_ITEM,
+    }, {
+      type: ActionType.CHANGE_GENRE_FILTER,
+      payload: `test genre`
+    })).toEqual({
+      films,
+      genres,
+      selectedGenre: `test genre`,
+    });
+  });
+
+  it(`Reducer should update films list`, () => {
+    expect(reducer({
+      films,
+      genres,
+      selectedGenre: DEFAULT_GENRE_ITEM,
+    }, {
+      type: ActionType.UPDATE_FILMS,
+      payload: [{name: `Test film`}]
+    })).toEqual({
+      films: [{name: `Test film`}],
+      genres,
+      selectedGenre: DEFAULT_GENRE_ITEM,
+    });
+  });
+
+  it(`Reducer should update genre list`, () => {
+    expect(reducer({
+      films,
+      genres,
+      selectedGenre: DEFAULT_GENRE_ITEM,
+    }, {
+      type: ActionType.UPDATE_GENRES_LIST,
+      payload: [`Test genre`]
+    })).toEqual({
+      films,
+      genres: [`Test genre`],
+      selectedGenre: DEFAULT_GENRE_ITEM,
+    });
+  });
+});
