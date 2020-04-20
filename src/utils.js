ce51000f6104191f0718103d69d0a5adf9a0e856
Nemhis <p.nemphis@gmail.com>
2020-04-21 00:38:13 +0300

    Format duration

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 export const formatDate = (date) => `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
 
 const MINUTES_PER_HOUR = 60;
+const SECOND_PER_MINUTE = 60;
 
 export const formatMinutes = (totalMinutes) => {
   let formatParts = [];
@@ -17,6 +18,30 @@ export const formatMinutes = (totalMinutes) => {
   }
 
   return formatParts.join(` `);
+};
+
+export const parseDuration = (totalSeconds) => {
+  const totalMinutes = Math.floor(totalSeconds / SECOND_PER_MINUTE);
+  const seconds = totalSeconds % SECOND_PER_MINUTE;
+
+  const hours = Math.floor(totalMinutes / MINUTES_PER_HOUR);
+  const minutes = totalMinutes % MINUTES_PER_HOUR;
+
+  return [
+    hours,
+    minutes,
+    seconds
+  ];
+};
+
+export const formatDuration = (durationInSec) => {
+  const [hours, minutes, seconds] = parseDuration(durationInSec);
+
+  return [
+    hours,
+    String(minutes).padStart(2, `0`),
+    String(seconds).padStart(2, `0`),
+  ].join(`:`);
 };
 
 export const extend = (a, b) => Object.assign({}, a, b);
